56f795289c86ff8d9a8ed2e324b90db0125e545f
KamonratNg <kamonrat.nga@gmail.com>
2020-11-07 17:04:00 +0700

    add more page

--- tirawat/urls.py
+++ tirawat/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import Home
+from .views import *
 
 urlpatterns = [
     path('', Home,name='home-page'),
+    path('home/', Home,name='home-page'),
+    path('about/', About,name='about-page'),
+    path('research/', About,name='research-page'),
+    path('service/', About,name='service-page'),
+    path('contact/', About,name='contact-page'),
 ]
--- tirawat/views.py
+++ tirawat/views.py
@@ -6,3 +6,17 @@ from django.http import HttpResponse
 
 def Home(request):
 	return render(request,'tirawat/home.html')
+
+def About(request):
+	return render(request,'tirawat/about.html')
+
+def Research(request):
+	return render(request,'tirawat/research.html')
+
+def Service(request):
+	return render(request,'tirawat/service.html')
+
+def Contact(request):
+	return render(request,'tirawat/contact.html')
+
+
